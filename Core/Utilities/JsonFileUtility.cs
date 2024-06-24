9ddf54ce168f90188382e61e4d43099102b652dd
xuand <xuandangvinh@gmail.com>
2024-06-24 22:39:16 +0700

    feat: create file constant and json file utility

---
+++ Core/Utilities/JsonFileUtility.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Text.Json;
+using Practice.Core.Extensions;
+
+namespace Practice.Core.Utilities;
+
+public class JsonFileUtility
+{
+    public static string ReadJsonFile(string path)
+    {
+        var filePath = StringExtensions.GetAbsolutePath(path);
+        return File.ReadAllText(filePath);
+    }
+    public static Dictionary<string, T> ReadAndParse<T>(string filePath)
+    {
+        var jsonData = File.ReadAllText(filePath);
+        var data = JsonSerializer.Deserialize<Dictionary<string, T>>(jsonData);
+        return data ?? new Dictionary<string, T>();
+    }
+}
---
+++ Test/Constants/FileConstant.cs
@@ -0,0 +1,8 @@
+﻿namespace Practice.Test.Constants;
+
+public class FileConstant
+{
+    public const string AccountFilePath = @"Test\TestData\Users\user.json";
+    public const string UpdateUserFilePath = @"Test\TestData\Users\user_update.json";
+    public const string SettingFilePath = @"Test\appsettings.json";
+}
