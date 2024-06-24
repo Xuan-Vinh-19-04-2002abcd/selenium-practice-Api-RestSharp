8b69f80ca8f628f6a0092633e6b9e3d07c71fa75
xuand <xuandangvinh@gmail.com>
2024-06-24 22:39:42 +0700

    feat: create get all user and get detail user test

---
+++ Test/TestCases/PracticeTest/GetAllUserTest.cs
@@ -0,0 +1,44 @@
+﻿using System.Net;
+using FluentAssertions;
+using Newtonsoft.Json.Linq;
+using Practice.Core;
+using Practice.Core.Extensions;
+using Practice.Core.Report;
+using Practice.Services.Service;
+
+namespace Practice.Test.TestCases.PracticeTest;
+
+public class GetAllUserTest : BaseTest
+{
+    private UserServices _userServices;
+
+    public GetAllUserTest() : base()
+    {
+        _userServices = new UserServices(ApiClient);
+    }
+    [Test]
+    public async Task GetAllUsersAsyncTest()
+    {
+        ReportLog.Info("1.Get ALl User");
+        var response = await _userServices.GetAllUser();
+        var result = response.Data;
+        ReportLog.Info("2.Vefify Status Code");
+        response.VerifyStatusCodeOk();
+        ReportLog.Info("3.Vefify Data");
+        result.Meta.Should().NotBeNull();
+        result.Meta.Pagination.Total.Should().BeGreaterThan(0, "because Total should be a positive number");
+        result.Meta.Pagination.Page.Should().BeGreaterThan(0, "because Page should be a positive number");
+        result.Meta.Pagination.Pages.Should().BeGreaterThan(0, "because Pages should be a positive number");
+        result.Meta.Pagination.Limit.Should().BeGreaterThan(0, "because Limit should be a positive number");
+        var expectedKeys = new List<string> { "id", "name", "email", "gender", "status" };
+        var listData = result.Data;
+        foreach (var item in listData)
+        { 
+            JObject user = JObject.FromObject(item);
+            foreach (var key in expectedKeys)
+            {
+                user.ContainsKey(key).Should().BeTrue($"because user should have the key '{key}'");
+            }
+        }
+    }
+}
---
+++ Test/TestCases/PracticeTest/GetDetailUserTest.cs
@@ -0,0 +1,50 @@
+﻿using FluentAssertions;
+using Practice.Core.Extensions;
+using Practice.Core.Report;
+using Practice.Services.Helper;
+using Practice.Services.Model.Request;
+using Practice.Services.Service;
+
+namespace Practice.Test.TestCases.PracticeTest;
+
+public class GetDetailUserTest : BaseTest
+{
+    private UserServices _userServices;
+
+    public GetDetailUserTest() : base()
+    {
+        _userServices = new UserServices(ApiClient);
+    }
+
+    [Test]
+    [TestCase("user_1")]
+    public async Task GetDetailUserAsyncTest(string userKey)
+    {
+        ReportLog.Info("1.Create User");
+        CreateUserDtoReq userReq = UserData[userKey];
+        var responseCreateUser = await _userServices.CreateUser(userReq);
+        var resultCreatUser = responseCreateUser.Data;
+        var idUser = resultCreatUser.Data.Id;
+        
+        UserHelper.StoreDataToDeleteUser(idUser);
+        
+        ReportLog.Info("2.Get Detail Users");
+        var responseGetDetailUser = await _userServices.GetUserDetail(idUser);
+        
+        ReportLog.Info("3.Verify response status code");
+        responseGetDetailUser.VerifyStatusCodeOk();
+        
+        ReportLog.Info("4.Verify response data");
+        var resultGetDetailUser = responseGetDetailUser.Data;
+        
+        ReportLog.Info("5.Verify status code");
+        responseGetDetailUser.Data.Code.Should().Be(200);
+        resultGetDetailUser.Data.Should().BeEquivalentTo(userReq, options => options
+            .ExcludingMissingMembers());
+    }
+    [TearDown]
+    public void TearDown()
+    {
+        UserHelper.DeleteUserFromStorage(_userServices);
+    }
+}
